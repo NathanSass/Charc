9bcf493761fd052bd999ed4ff011d28cffa39ffd
Nathan Sass <nsass711@gmail.com>
2016-10-31 10:23:34 -0700

    the wobbles and walks the length of the activity.

--- app/src/main/java/com/nathansass/charc/fragments/AnimationFragment.java
+++ app/src/main/java/com/nathansass/charc/fragments/AnimationFragment.java
@@ -1,12 +1,17 @@
 package com.nathansass.charc.fragments;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
@@ -37,9 +42,127 @@ public class AnimationFragment extends Fragment {
         binding.btnAnimate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                animateAnt(binding.ivAnt);
+                walkUpScreen();
             }
         });
+
+    }
+
+    private void antWobble() {
+        int speed = 200;
+        int rotation = 8;
+        ObjectAnimator wobbleLeft =
+                ObjectAnimator.ofFloat(binding.ivAnt, View.ROTATION, -rotation);
+        wobbleLeft.setDuration(speed);
+        wobbleLeft.setInterpolator(new DecelerateInterpolator());
+
+        ObjectAnimator wobbleRight =
+                ObjectAnimator.ofFloat(binding.ivAnt, View.ROTATION, rotation);
+        wobbleRight.setDuration(speed);
+        wobbleRight.setInterpolator(new DecelerateInterpolator());
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playSequentially(wobbleLeft, wobbleRight);
+        animatorSet.start();
+
+        animatorSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animation.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+    }
+
+    private void antMoveSideToSide() {
+        int speed = 300;
+        int dist = 5;
+        ObjectAnimator leftShuffle = ObjectAnimator.ofFloat(binding.ivAnt, View.TRANSLATION_X, -dist);
+        leftShuffle.setDuration(speed);
+        leftShuffle.setInterpolator(new DecelerateInterpolator());
+
+        ObjectAnimator rightShuffle = ObjectAnimator.ofFloat(binding.ivAnt, View.TRANSLATION_X, dist);
+        rightShuffle.setDuration(speed);
+        rightShuffle.setInterpolator(new DecelerateInterpolator());
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.playSequentially(leftShuffle, rightShuffle);
+        animatorSet.start();
+
+        animatorSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animation.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+    }
+
+    private void walkUpScreen() {
+        int time = 3000;
+
+        ObjectAnimator moveSide =
+                ObjectAnimator.ofFloat(binding.ivAnt, View.TRANSLATION_X, 0); //Change to 400 later
+        moveSide.setDuration(time);
+
+        ObjectAnimator moveAhead =
+                ObjectAnimator.ofFloat(binding.ivAnt, View.TRANSLATION_Y, -getScreenHeight() + 400);
+        moveAhead.setDuration(time);
+
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                antWobble();
+                antMoveSideToSide();
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animation.cancel(); //stops the ant for wobbling
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+        animatorSet.playTogether(moveSide, moveAhead);
+        animatorSet.start();
+
     }
 
     private void animateAnt(ImageView imageView) {
@@ -51,5 +174,12 @@ public class AnimationFragment extends Fragment {
         //animation.setFillAfter(true);
 
         imageView.startAnimation(animation);  // start animation
+    }
+
+    private int getScreenHeight() {
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        int height = displaymetrics.heightPixels;
+        return  height;
     }
 }
